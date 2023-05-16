70e5f947e15a983863afc3c202cc46b00af4a227
scott chiang <scott01272001@gmail.com>
2023-05-17 07:43:37 +0800

    add db docker config

--- pre-required.sql
+++
@@ -1,1 +0,0 @@
-create database line_bot;
--- src/test/java/com/scott/sideproject/linebotbackend/LineBotBackendApplicationTests.java
+++ src/test/java/com/scott/sideproject/linebotbackend/LineBotBackendApplicationTests.java
@@ -1,11 +1,6 @@
 package com.scott.sideproject.linebotbackend;
 
-import org.checkerframework.checker.units.qual.A;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @SpringBootTest
 public class LineBotBackendApplicationTests {
